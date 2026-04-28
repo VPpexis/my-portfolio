6685b1a015f87119d091393e1e2420deb5e30705
VPpexis <panuganvanphilip@gmail.com>
2026-04-28 12:23:34 +0800

    fix: update PostCSS and Tailwind configuration files
    - Refactored postcss.config.mjs to use default export and corrected plugin syntax.
    - Added new Tailwind CSS configuration file (tailwind.config.ts) with basic setup.
    - Updated tsconfig.json to include eslint.config.mts in the include array.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,51 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tselint from 'typescript-eslint';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import sanityConfig from '@sanity/eslint-config-studio';
+import unusedImports from 'eslint-plugin-unused-imports';
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+const eslintConfig = tselint.config([
+   js.configs.recommended,
+   ...tselint.configs.recommended,
+    ...nextVitals,
+    {
+        plugins: {
+            'unused-imports': unusedImports,
+        }
+    },
+    {
+        files: ['studio/**/*.{ts,tsx,js,jsx}'],
+        ...sanityConfig,
+    },
+    {
+        rules: {
+            'no-unused-vars': 'off',
+            '@typescript-eslint/no-unused-vars': 'off',
+            'unused-imports/no-unused-imports': 'error',
+            'unused-imports/no-unused-vars': [
+                'warn',
+                {
+                    vars: 'all',
+                    varsIgnorePattern: '^_',
+                    args: 'after-used',
+                    argsIgnorePattern: '^_',
+                },
+            ],
+        }
+    },
+    {
+        ignores: [
+            '.next/**',
+            'dist/**',
+            'build/**',
+            'out/**',
+            'coverage/**',
+            'node_modules/**',
+            'public/**',
+            'next-env.d.ts',
+        ],
+    },
+])
 
 export default eslintConfig;
